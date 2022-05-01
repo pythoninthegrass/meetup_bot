1a8359ae48ac8c57d114e5f7e3b6e80416b4d5fc
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-04-30 20:14:44 -0500

    Removed knockknock, moved channel to .env, basic message POST

--- slack.py
+++ slack.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 
+import json
 import os
 import requests
-import requests_cache
-import tqdm
 from decouple import config
-from knockknock import slack_sender
 from pathlib import Path
 
 # env
@@ -13,44 +11,34 @@ home = Path.home()
 env = Path('.env')
 cwd = Path.cwd()
 
-# verbose icecream
-# ic.configureOutput(includeContext=True)
-
-requests_cache.install_cache("api_cache")
-
 # creds
 if env.exists():
     BOT_USER_TOKEN = config('BOT_USER_TOKEN')
     SLACK_WEBHOOK = config('SLACK_WEBHOOK')
+    CHANNEL = config('CHANNEL')
 else:
     BOT_USER_TOKEN = os.getenv('BOT_USER_TOKEN')
     SLACK_WEBHOOK = os.getenv('SLACK_WEBHOOK')
+    CHANNEL = os.getenv('CHANNEL')
 
 webhook_url = SLACK_WEBHOOK
 
 
-def isprime(n):
-    if n == 2:
-        return True
-    if n < 2 or n % 2 == 0:
-        return False
-    for i in range(3, int(n**0.5)+1, 2):
-        if n % i == 0:
-            return False
-    return True
+def send_message(message):
+    """
+    Send a message to Slack
+    """
+    # curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' $SLACK_WEB_HOOK
+    payload = {
+        "channel": CHANNEL,
+        "text": message
+    }
+    response = requests.post(
+        webhook_url,
+        data=json.dumps(payload),
+        headers={'Content-Type': 'application/json'}
+    )
+    return response
 
 
-@slack_sender(webhook_url = webhook_url, channel="knockknock")
-def get_prime_numbers(max_value):
-
-    final_prime_numbers = []
-
-    for num in tqdm(range(max_value)):
-
-      if(isprime(num)):
-        final_prime_numbers.append(num)
-
-    return final_prime_numbers
-
- # Run the function with a value of 20
-get_prime_numbers(20)
+# send_message()
