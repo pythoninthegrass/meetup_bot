d56e110c1a235029e224bd0731d80b91322cc269
Alex Ayon <jayonloza@gmail.com>
2022-06-01 20:05:28 -0500

    add node jwt test file

--- jsonwebtoken_test/test.js
+++ jsonwebtoken_test/test.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
-const privateKey = fs.readFileSync('../jwt_priv.pem');
+const privateKey = fs.readFileSync('jwt_priv.pem');
 
 // source .env file one directory up
-const envFile = require('dotenv').config({path: '../.env'});
+const envFile = require('dotenv').config({path: '.env'});
 
-CLIENT_ID = process.env.CLIENT_ID;
-CLIENT_SECRET = process.env.CLIENT_SECRET;
-SIGNING_KEY_ID = process.env.SIGNING_KEY_ID;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const SIGNING_KEY_ID = process.env.SIGNING_KEY_ID;
 
 //   {
 //     algorithm: 'RS256',
@@ -18,7 +18,7 @@ SIGNING_KEY_ID = process.env.SIGNING_KEY_ID;
 //     expiresIn: 120
 //   }
 
-jwt.sign(
+const token = jwt.sign(
   {},
   privateKey,
   {
@@ -30,3 +30,5 @@ jwt.sign(
     expiresIn: 120
   }
 );
+
+console.log(token);
