d8511fa5372a9c5880bfb24b9f8426a91f1b0036
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-06-26 18:17:45 -0500

    Clean up unused code

--- capture_groups.py
+++ capture_groups.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-# import re
 from icecream import ic
-# from pathlib import Path
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 """
@@ -16,9 +14,6 @@ Works around that by scraping the HTML of the groups page and extracting the gro
 ic.configureOutput(includeContext=True)
 
 # scrape the url for subdomain (i.e., 'okc-fp')
-# https://www.meetup.com/okc-fp/
-# url = "https://www.meetup.com/find/?suggested=true&source=GROUPS&keywords=programming&categoryId=546&distance=tenMiles&location=us--ok--Oklahoma%20City"
-# url = "https://www.meetup.com/find/"
 url = "https://www.meetup.com/find/?distance=tenMiles&source=GROUPS&categoryId=546"
 
 
@@ -44,45 +39,8 @@ def run(playwright: Playwright) -> None:
     # Go to https://www.meetup.com/find/
     page.goto(url)
 
-    # Click [placeholder="Enter location"]
-    # page.locator("[placeholder=\"Enter location\"]").click()
-
-    # Fill [placeholder="Enter location"]
-    # page.locator("[placeholder=\"Enter location\"]").fill("oklahoma city")
-
-    # Click text=Oklahoma City, Oklahoma, USA
-    # page.locator("text=Oklahoma City, Oklahoma, USA").click()
-
-    # expect(page).to_have_url("https://www.meetup.com/find/?location=us--ok--Oklahoma%20City&source=EVENTS")
-
-    # Click [placeholder="Search for keywords"]
-    # page.locator("[placeholder=\"Search for keywords\"]").click()
-
-    # Fill [placeholder="Search for keywords"]
-    # page.locator("[placeholder=\"Search for keywords\"]").fill("programming")
-
-    # Click text=Any distanceicon
-    # page.locator("text=Any distanceicon").click()
-
-    # Click text=10 miles
-    # page.locator("text=10 miles").click()
-    # expect(page).to_have_url("https://www.meetup.com/find/?location=us--ok--Oklahoma%20City&source=EVENTS&distance=tenMiles")
-
-    # Click text=Any categoryicon
-    # page.locator("text=Any categoryicon").click()
-
-    # Click text=Technology
-    # page.locator("text=Technology").click()
-    # expect(page).to_have_url("https://www.meetup.com/find/?location=us--ok--Oklahoma%20City&source=EVENTS&distance=tenMiles&categoryId=546")
-
-    # Click [data-testid="GROUPS"]
-    # page.locator("[data-testid=\"GROUPS\"]").click()
-    # expect(page).to_have_url("https://www.meetup.com/find/?location=us--ok--Oklahoma%20City&source=GROUPS&distance=tenMiles&categoryId=546")
-
     # xpath
     # //*[@id="group-card-in-search-results"]
-
-    # regex = re.compile(r'"https:\/\/www\.meetup\.com\/(.*?)\/"')
 
     handles = []
 
@@ -93,10 +51,6 @@ def run(playwright: Playwright) -> None:
 
     context.close()
     browser.close()
-
-    # URLs only
-    # for i in handles:
-    #     print(i)
 
     # dataframe of URLs
     df = pd.DataFrame(handles)
