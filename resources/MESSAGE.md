8991ddb595bd7f1e22311f90f8c04bd7f724fa43
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-07-05 01:17:39 -0500

    Slack API + message formatting
    Slack SDK is the winner with some quirks in queries and markdown implementation. Worked around it with manual channel IDs and pandas

---
+++ resources/MESSAGE.md
@@ -0,0 +1,8 @@
+* Thu 7/7 at 7:00 pm OK Game Devs <https://www.meetup.com/oklahoma-game-developers/events/286544018/|Virtual Demo Day> on Twitch
+* Fri 7/8 at 12:30 pm <https://www.meetup.com/okccoffeeandcode/events/286920205/|Gray Owl Coffee and Code>
+* Sat 7/9 at 2:00 pm <https://www.meetup.com/okccoffeeandcode/events/286873531/|10th Street Kamp's Coffee and Code>
+* Sat 7/9 at 2:00 pm OKC OSH <https://www.meetup.com/okc-osh/events/286777681/|Hybrid In Person and Virtual Hangout!> at Kamp's and on Zoom
+* Sat 7/16 at 5:00 pm <https://www.meetup.com/okccoffeeandcode/events/286873269/|8th Street Meet and Greet>
+* Tue 7/19 at 11:30 am <https://www.meetup.com/okcwebdevs/events/286713247/|A Practical Take on Web3> with John Mosesman at Clevyr
+* Thu 7/21 at 6:00 pm <https://www.meetup.com/nerdbeers/events/286591587/|Nerd Beers at Parlor OKC> - Alcohol: optional. Geekiness: required.  
+We're also taking suggestions and sometimes planning impromptu meetups for the <https://www.meetup.com/okccoffeeandcode/|OKC Coffee and Code> group in the #okc-metro channel!
--- slack.py
+++ slack.py
@@ -1,48 +1,191 @@
 #!/usr/bin/env python3
 
 import json
+# import logging
 import os
-import requests
+import pandas as pd
+# import requests
+# import time
 from decouple import config
+from icecream import ic
+# from markdown import markdown
 from pathlib import Path
+# from slack_bolt import App
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# logging.basicConfig(level=logging.DEBUG)
+
+# verbose icecream
+ic.configureOutput(includeContext=True)
+
+# pandas don't truncate output
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_colwidth', None)
 
 # env
 home = Path.home()
 env = Path('.env')
 cwd = Path.cwd()
+# format = 'json'
+csv_fn = Path('raw/output.csv')
+json_fn = Path('raw/output.json')
+groups_csv = Path('raw/groups.csv')
 
 # creds
 if env.exists():
+    USER_TOKEN = config('USER_TOKEN')
     BOT_USER_TOKEN = config('BOT_USER_TOKEN')
     SLACK_WEBHOOK = config('SLACK_WEBHOOK')
+    SIGNING_SECRET = config('SIGNING_SECRET')
     ENDPOINT = config('ENDPOINT')
     CHANNEL = config('CHANNEL')
+    PORT = config('BOLT_PORT', default=3001, cast=int)
 else:
+    USER_TOKEN = os.getenv('USER_TOKEN')
     BOT_USER_TOKEN = os.getenv('BOT_USER_TOKEN')
     SLACK_WEBHOOK = os.getenv('SLACK_WEBHOOK')
+    SIGNING_SECRET = os.getenv('SIGNING_SECRET')
     ENDPOINT = os.getenv('ENDPOINT')
     CHANNEL = os.getenv('CHANNEL')
+    PORT = int(os.getenv('BOLT_PORT'))
 
-webhook_url = SLACK_WEBHOOK
-base_url = "https://slack.com/api/"
-endpoint = base_url + ENDPOINT
+# python sdk
+client = WebClient(token=USER_TOKEN)
+
+# read manually entered 'raw/channels.csv'
+chan = pd.read_csv('raw/channels.csv')
+
+# locate id from `CHANNEL` name
+channel_id = chan[chan['name'] == CHANNEL]['id'].values[0]
+
+# convo_list = client.conversations_list()
+# # pandas read list of dictionaries
+# df = pd.DataFrame(convo_list["channels"])
+# # export response to json
+# df.to_json('raw/channels.json', orient='records')
+
+
+def fmt_json(filename):
+    # read json file
+    data = json.load(open(filename))
+
+    # create dataframe
+    df = pd.DataFrame(data)
+
+    # add column: 'message' with date, name, title, eventUrl
+    df['message'] = df.apply(lambda x: f'• {x["date"]} *{x["name"]}* <{x["eventUrl"]}|{x["title"]}> ', axis=1)
+
+    # convert message to a single string
+    # msg = df['message'].to_string(index=False)
+
+    # convert message column to list of strings (avoids alignment shenanigans)
+    msg = df['message'].tolist()
+
+    return msg
 
 
 def send_message(message):
-    """
-    Send a message to Slack
-    """
-    # curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' $SLACK_WEB_HOOK
-    payload = {
-        "channel": CHANNEL,
-        "text": message
-    }
-    response = requests.post(
-        webhook_url,
-        data=json.dumps(payload),
-        headers={'Content-Type': 'application/json'}
-    )
+    try:
+        response = client.chat_postMessage(
+            channel=channel_id,
+            text="",
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": message
+                    }
+                }
+            ]
+        )
+    except SlackApiError as e:
+        assert e.response["error"]
+
     return response
 
 
+def main():
+    # msg = \
+    # """
+    # <https://github.com/leachim6/hello-world/wiki/|Hello world!> :python2: :point_right: :joy:
+    # """
+
+    # # read md file
+    # md = Path('resources/MESSAGE.md')
+    # with open(md, 'r') as f:
+    #     msg = f.read()
+
+    # # https://api.slack.com/reference/surfaces/formatting#block-formatting
+    # # replace '*' with '•' in `msg`
+    # msg = msg.replace('*', '•')
+
+    msg = fmt_json(json_fn)
+
+    # send message as one concatenated string
+    send_message('\n'.join(msg))
+
+
+if __name__ == '__main__':
+    main()
+
+
+# REQUESTS
+
+# webhook_url = SLACK_WEBHOOK
+# base_url = "https://slack.com/api/"
+# endpoint = base_url + ENDPOINT
+
+# def send_message(message):
+#     """
+#     Send a message to Slack
+#     """
+#     # curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' $SLACK_WEB_HOOK
+#     payload = {
+#         "channel": CHANNEL,
+#         "text": message
+#     }
+#     response = requests.post(
+#         webhook_url,
+#         data=json.dumps(payload),
+#         headers={'Content-Type': 'application/json'}
+#     )
+#     return response
+
+
 # send_message()
+
+
+# BOLT
+
+# # Initializes your app with your bot token and signing secret
+# app = App(
+#     token=BOT_USER_TOKEN,
+#     signing_secret=SIGNING_SECRET
+# )
+
+
+# # Listens to incoming messages that contain "hello"
+# @app.message(":python2:")
+# def message_hello(message, say):
+#     """say() sends a message to the channel where the event was triggered"""
+#     say(
+#         blocks=[
+#             {
+#                 "type": "section",
+#                 "text": {"type": "mrkdwn", "text": f"Hey there <@{message['user']}>!"},
+#                 "accessory": {
+#                     "type": "button",
+#                     "text": {"type": "plain_text", "text": "Click Me"},
+#                     "action_id": "button_click"
+#                 }
+#             }
+#         ],
+#         text=f"Bienvenue <@{message['user']}>!"
+#     )
+
+
+# if __name__ == "__main__":
+#     app.start(port=PORT)
