61d4bb898fc13f564ffaffc571a4b56c3b278f08
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2022-09-10 00:37:50 -0500

    Removed rocketry import/functions
    Will test locally

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ from passlib.context import CryptContext
 from pathlib import Path
 from pony.orm import *
 from pydantic import BaseModel
-from scheduler import app as app_rocketry, do_things
 from sign_jwt import main as gen_token
 from slackbot import *
 
@@ -117,32 +116,7 @@ app.add_middleware(
 Scheduler
 """
 
-session = app_rocketry.session
-
-
-@app.post("/session/shut_down")
-async def shut_down_session():
-    "Shut down the scheduler"
-    session.shut_down()
-
-
-@app.post("/tasks/{task_name}/run")
-async def run_task(task_name:str):
-    "Run given task"
-    task = session[task_name]
-    task.force_run = True
-
-
-@app.get("/tasks")
-async def read_tasks():
-    return list(session.tasks)
-
-
-@app.get("/logs")
-async def read_logs():
-    "Get task logs"
-    repo = session.get_repo()
-    return repo.filter_by().all()
+# TODO: rocketry or wakaq
 
 
 """
