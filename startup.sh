f00daa62c8bec23bb2f0d42d7ad69b93cb2698aa
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-01-22 14:56:12 -0600

    Quote port var

--- startup.sh
+++ startup.sh
@@ -9,4 +9,4 @@ export PATH="${VIRTUAL_ENV}/bin:$HOME/.asdf/bin:$HOME/.asdf/shims:$PATH"
 
 # source .venv/bin/activate
 
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app -b 0.0.0.0:${PORT:-3000} --log-file -
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app -b "0.0.0.0:${PORT:-3000}" --log-file -
