fba3ac730f9a8eb0ddedc6c1e65f84a40897ef9e
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-01-22 18:21:08 -0600

    Test time

--- scheduler.sh
+++ scheduler.sh
@@ -44,12 +44,12 @@ send_request() {
 		--data-urlencode "exclusions=36\u00b0N,Tulsa,Nerdy Girls"
 }
 
-# TODO: remove 2300 UTC (i.e., 5pm CST) to only send once per day
+# TODO: remove 2300 UTC (i.e., 5pm CST) to only send once per day (2330 is a test)
 # healthchecks ID: 1500 and 2300 UTC
 if [ $(date -u +%H%M) -eq "1500" ]; then
   HEALTHCHECKS_ID="02695fa4-3775-4a52-bd05-1db9883b079f"
   send_request
-elif [ $(date -u +%H%M) -eq "2300" ]; then
+elif [ $(date -u +%H%M) -eq "2330" ]; then
   HEALTHCHECKS_ID="9f0093c0-6519-4989-b7e8-a12b250c58dd"
   send_request
 else
