7d7004782e3b909dc984ebb6f8bbeb4601e340e0
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-02-05 15:42:57 -0600

    SC exclusions, rename vars, dot source
    Latter for sh portability

--- startup.sh
+++ startup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
-if [ "$(uname -s)" = "Darwin" ]; then
-	export VIRTUAL_ENV=".venv"
-else
-	export VIRTUAL_ENV="/opt/venv"
-fi
-export PATH="${VIRTUAL_ENV}/bin:$HOME/.asdf/bin:$HOME/.asdf/shims:$PATH"
+# shellcheck disable=SC1091,SC2034,SC2153,SC3037,SC3045,SC2046
 
-# source .venv/bin/activate
+if [ "$(uname -s)" = "Darwin" ]; then
+	export VENV=".venv"
+else
+	export VENV="/opt/venv"
+fi
+export PATH="${VENV}/bin:$HOME/.asdf/bin:$HOME/.asdf/shims:$PATH"
+
+# . "${VENV}/bin/activate"
 
 gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app -b "0.0.0.0:${PORT:-3000}" --log-file -
